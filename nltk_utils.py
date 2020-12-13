3b172a1c43b5604b09ab80469a42b4068439a881
JCharis <jcharistech@gmail.com>
2020-12-13 15:42:45 +0200

    Added download_corpora To Utils

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,7 +1,11 @@
-import nltk
+# import nltk
 
-nltk.download('wordnet')
-nltk.download('punkt')
-nltk.download('tagsets')
-nltk.download('treebank')
-nltk.download('all-corpora')
+# nltk.download('wordnet')
+# nltk.download('punkt')
+# nltk.download('tagsets')
+# nltk.download('treebank')
+# nltk.download('all-corpora')
+import subprocess
+cmd = ['python3','-m' ,'textblob.download_corpora' ]
+subprocess.run(cmd)
+print("Working")
