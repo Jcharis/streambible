fe8ddf309b4a5d350d7ecb3d304dfb9d78db70fe
JCharis <jcharistech@gmail.com>
2020-12-13 15:01:53 +0200

    Updated App

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ import pandas as pd
 # NLP Pkgs
 import spacy
 from spacy import displacy
-nlp = spacy.load('en')
+nlp = spacy.load('en_core_web_sm') # Fixes Error For Deployment for shortlink
 from textblob import TextBlob
 from collections import Counter
 
